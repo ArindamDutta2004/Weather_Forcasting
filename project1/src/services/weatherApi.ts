1ffd564e677695f22a7dc1b99eaa4e05cefb4bb1
ArindamDutta2004 <113087493+ArindamDutta2004@users.noreply.github.com>
2025-07-17 16:40:36 +0530

    Update weatherApi.ts

--- project1/src/services/weatherApi.ts
+++ project1/src/services/weatherApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { CurrentWeather, ForecastData } from '../types/weather';
 
-const API_KEY = process.env.API_KEY; // Your working OpenWeatherMap API key
+const API_KEY = '3f5f3e1e2acc15b98dec7117aee33736'; // Your working OpenWeatherMap API key
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 class WeatherAPI {
